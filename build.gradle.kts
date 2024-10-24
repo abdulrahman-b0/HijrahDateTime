025cdf01749d92683fb939ec7c27eb5ff739d931
Abdulrahman <Abdulrahman-B0@hotmail.com>
2024-10-24 18:53:30 +0300

    added maven publish, and changed kotlin jvm target to 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
+    id("maven-publish")
 }
 
 group = "com.abdulrahman-b"
@@ -10,20 +13,22 @@ repositories {
     mavenCentral()
 }
 
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
     implementation(libs.kotlin.reflect)
     implementation(libs.kotlin.serialization.json)
-    implementation(libs.slf4j.api)
     testImplementation(libs.junit.jupiter.api)
     testImplementation(libs.junit.jupiter)
-    testImplementation(libs.truth)
 }
 
 tasks.test {
