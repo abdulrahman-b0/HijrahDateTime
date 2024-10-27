5f59ec10a4e0bd8f264d59ddd28181dd770632f5
Abdulrahman <Abdulrahman-B0@hotmail.com>
2024-10-27 22:31:48 +0300

    Added missing docs

--- src/main/kotlin/com/abdulrahman_b/hijrahDateTime/formats/HijrahFormatters.kt
+++ src/main/kotlin/com/abdulrahman_b/hijrahDateTime/formats/HijrahFormatters.kt
@@ -5,6 +5,8 @@ package com.abdulrahman_b.hijrahDateTime.formats
 import com.abdulrahman_b.hijrahDateTime.time.HijrahDateTime
 import com.abdulrahman_b.hijrahDateTime.time.OffsetHijrahDateTime
 import com.abdulrahman_b.hijrahDateTime.time.ZonedHijrahDateTime
+import java.time.ZoneId
+import java.time.ZoneOffset
 import java.time.chrono.HijrahChronology
 import java.time.chrono.HijrahDate
 import java.time.format.DateTimeFormatter
@@ -50,17 +52,26 @@ object HijrahFormatters {
         .toFormatter(ResolverStyle.STRICT, HijrahChronology.INSTANCE)
 
 
-    val HIJRAH_ZONED_DATE: DateTimeFormatter = DateTimeFormatterBuilder()
-        .parseCaseInsensitive()
-        .append(HIJRAH_DATE)
-        .optionalStart()
-        .appendOffsetId()
-        .toFormatter(ResolverStyle.STRICT, HijrahChronology.INSTANCE)
 
-
+    /**
+     * The Hijrah date formatter that formats or parses a date with an
+     * offset, such as '1446-04-18+03:00'.
+     *
+     * This returns an immutable formatter capable of formatting and parsing
+     * the Hijrah extended offset date format.
+     * The format consists of:
+     *
+     * * The [HIJRAH_DATE]
+     * * The [ZoneOffset.getId]. If the offset has seconds then they will be handled. Optional.
+     *
+     * The returned formatter has a chronology of [HijrahChronology] set to ensure dates in
+     * other calendar systems are correctly converted.
+     * It has no override zone and uses the [ResolverStyle.STRICT] resolver style.
+     */
     val HIJRAH_OFFSET_DATE: DateTimeFormatter = DateTimeFormatterBuilder()
         .parseCaseInsensitive()
         .append(HIJRAH_DATE)
+        .optionalStart()
         .appendOffsetId()
         .toFormatter(ResolverStyle.STRICT, HijrahChronology.INSTANCE)
 
@@ -86,18 +97,24 @@ object HijrahFormatters {
         .append(ISO_LOCAL_TIME)
         .toFormatter(ResolverStyle.STRICT, HijrahChronology.INSTANCE)
 
-    val HIJRAH_ZONED_DATE_TIME: DateTimeFormatter = DateTimeFormatterBuilder()
-        .append(HIJRAH_DATE_TIME)
-        .optionalStart()
-        .appendOffsetId()
-        .optionalStart()
-        .appendLiteral('[')
-        .parseCaseSensitive()
-        .appendZoneRegionId()
-        .appendLiteral(']')
-        .toFormatter(ResolverStyle.STRICT, HijrahChronology.INSTANCE)
 
-
+    /**
+     * The Hijrah date-time formatter that formats or parses a date-time with an
+     * offset, such as '1446-12-18T10:15:30+03:00'.
+     *
+     * This returns an immutable formatter capable of formatting and parsing
+     * the Hijrah extended offset date-time format.
+     * The format consists of:
+     *
+     * * The [HIJRAH_DATE_TIME] formatter.
+     * * The [ZoneOffset.getId]. If the offset has seconds then they will be handled.
+     *  The offset parsing is lenient, which allows the minutes and seconds to be optional.
+     *  Parsing is case insensitive.
+     *
+     * The returned formatter has a chronology of Hijrah set to ensure dates in
+     * other calendar systems are correctly converted.
+     * It has no override zone and uses the [ResolverStyle.STRICT] resolver style.
+     */
     val HIJRAH_OFFSET_DATE_TIME: DateTimeFormatter = DateTimeFormatterBuilder()
         .parseCaseInsensitive()
         .append(HIJRAH_DATE_TIME)
@@ -106,10 +123,46 @@ object HijrahFormatters {
         .parseStrict()
         .toFormatter(ResolverStyle.STRICT, HijrahChronology.INSTANCE)
 
-    fun DateTimeFormatterBuilder.toFormatter(resolverStyle: ResolverStyle, chronology: HijrahChronology, locale: Locale = Locale.getDefault()): DateTimeFormatter {
+    /**
+     * The Hijrah date-time formatter that formats or parses a date-time with
+     * offset and zone, such as '1446-04-18T10:15:30+03:00[Asia/Riyadh]'.
+     *
+     * This returns an immutable formatter capable of formatting and parsing
+     * a format that extends the Hijrah offset date-time format
+     * to add the time-zone.
+     * The section in square brackets is not part of the ISO-8601 standard.
+     * The format consists of:
+     *
+     * * The [HIJRAH_OFFSET_DATE_TIME] formatter.
+     * * If the zone ID is not available or is a [ZoneOffset] then the format is done.
+     * * An open square bracket '['.
+     * * The [ZoneId.getId].
+     *  Parsing is case sensitive.
+     * * A close square bracket ']'.
+     *
+     * The returned formatter has a chronology of Hijrah set to ensure dates in
+     * other calendar systems are correctly converted.
+     * It has no override zone and uses the [ResolverStyle.STRICT] resolver style.
+     */
+    val HIJRAH_ZONED_DATE_TIME: DateTimeFormatter = DateTimeFormatterBuilder()
+        .append(HIJRAH_OFFSET_DATE_TIME)
+        .optionalStart()
+        .appendLiteral('[')
+        .parseCaseSensitive()
+        .appendZoneRegionId()
+        .appendLiteral(']')
+        .toFormatter(ResolverStyle.STRICT, HijrahChronology.INSTANCE)
+
+    internal fun DateTimeFormatterBuilder.toFormatter(resolverStyle: ResolverStyle, chronology: HijrahChronology, locale: Locale = Locale.getDefault()): DateTimeFormatter {
         return this.toFormatter().withChronology(chronology).withResolverStyle(resolverStyle).withLocale(locale)
     }
 
+    /**
+     * Returns the recommended formatter for the specified temporal object.
+     *
+     * @param temporal the temporal object to get the recommended formatter for.
+     * @return the recommended formatter for the specified temporal object.
+     */
     @Throws(UnsupportedTemporalTypeException::class)
     fun getRecommendedFormatter(temporal: Temporal): DateTimeFormatter {
         return when (temporal) {
