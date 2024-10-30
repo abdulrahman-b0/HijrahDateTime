ead40ce3f5d13f82bb75f1ca901187433938b8bd
Abdulrahman <Abdulrahman-B0@hotmail.com>
2024-10-30 17:01:46 +0300

    Added extra validation in some tests

--- src/test/kotlin/com/abdulrahman_b/hijrahDateTime/time/extensions/InstantExtensionsKtTest.kt
+++ src/test/kotlin/com/abdulrahman_b/hijrahDateTime/time/extensions/InstantExtensionsKtTest.kt
@@ -1,14 +1,14 @@
 package com.abdulrahman_b.hijrahDateTime.time.extensions
 
-import com.abdulrahman_b.hijrahDateTime.formats.HijrahFormatters.HIJRAH_OFFSET_DATE_TIME
-import com.abdulrahman_b.hijrahDateTime.formats.HijrahFormatters.HIJRAH_ZONED_DATE_TIME
+import com.abdulrahman_b.hijrahDateTime.time.OffsetHijrahDateTime
 import com.abdulrahman_b.hijrahDateTime.time.ZonedHijrahDateTime
+import com.abdulrahman_b.hijrahDateTime.time.extensions.DateTimeConversions.toOffsetHijrahDateTime
+import com.abdulrahman_b.hijrahDateTime.time.extensions.DateTimeConversions.toZonedHijrahDateTime
 import com.abdulrahman_b.hijrahDateTime.time.extensions.Instants.atHijrahOffset
 import com.abdulrahman_b.hijrahDateTime.time.extensions.Instants.atHijrahZone
 import com.abdulrahman_b.hijrahDateTime.time.extensions.Instants.toHijrahString
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
-
 import org.junit.jupiter.api.Test
 import java.time.Instant
 import java.time.ZoneOffset
@@ -27,18 +27,28 @@ class InstantExtensionsKtTest {
     fun atZoneHijrah() {
 
         val zonedHijrahDateTime = instant.atHijrahZone(ZoneOffset.UTC)
-        val expected = "1446-02-05T12:43:18Z"
+        val expected = ZonedHijrahDateTime.of(1446, 2, 5, 12, 43, 18, 0, ZoneOffset.UTC)
 
-        assertEquals(expected, zonedHijrahDateTime.format(HIJRAH_ZONED_DATE_TIME))
+        assertEquals(expected, zonedHijrahDateTime)
+
+        val localDateTime = instant.atZone(ZoneOffset.UTC)
+        val asHijrah = localDateTime.toZonedHijrahDateTime()
+
+        assertEquals(expected, asHijrah) //Extra validation
     }
 
     @Test
     fun atOffsetHijrah() {
 
         val offsetHijrahDateTime = instant.atHijrahOffset(ZoneOffset.UTC)
-        val expected = "1446-02-05T12:43:18Z"
+        val expected = OffsetHijrahDateTime.of(1446, 2, 5, 12, 43, 18, 0, ZoneOffset.UTC)
 
-        assertEquals(expected, offsetHijrahDateTime.format(HIJRAH_OFFSET_DATE_TIME))
+        assertEquals(expected, offsetHijrahDateTime)
+
+        val localDateTime = instant.atOffset(ZoneOffset.UTC)
+        val asHijrah = localDateTime.toOffsetHijrahDateTime()
+
+        assertEquals(expected, asHijrah) //Extra validation
     }
 
     @Test
