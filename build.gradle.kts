52b2ec121a26bd111769b983984552dd41450747
Abdulrahman <Abdulrahman-B0@hotmail.com>
2024-10-31 14:44:11 +0300

    deleted Kdoc

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.dokka)
     alias(libs.plugins.jreleaser)
     `maven-publish`
 }
