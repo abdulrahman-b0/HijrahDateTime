aa53ba17e326ed0a1b0e98d269df4c83e7b0f35f
Abdulrahman Bahamel <Abdulrahman-b0@hotmail.com>
2026-03-24 16:43:05 +0300

    🔥 Remove Android-specific `HijrahDate.format` implementation to clean up platform-specific code.

--- library/src/androidMain/kotlin/com/abdulrahman_b/hijrahdatetime/HijrahDate.android.kt
+++
@@ -1,7 +0,0 @@
-package com.abdulrahman_b.hijrahdatetime
-
-import com.abdulrahman_b.hijrahdatetime.format.HijrahDateTimeFormat
-
-actual fun HijrahDate.format(format: HijrahDateTimeFormat): String {
-    return format.javaFormatter.withChronology(javaDate.chronology).format(javaDate)
-}
