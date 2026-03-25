de9f05d9e4bba17b2caf62ef10970c96aac1e2c7
Abdulrahman Bahamel <Abdulrahman-b0@hotmail.com>
2026-03-25 09:37:57 +0300

    🔖 Bump version to `2.0.0-alpha04` and update Maven Publish plugin:
    - Updated project version in `build.gradle.kts`.
    - Upgraded `com.vanniktech.maven.publish` plugin to `0.36.0` in `libs.versions.toml`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 rootProject.group = "com.abdulrahman-b.hijrahdatetime"
-rootProject.version = "2.0.0-alpha03"
+rootProject.version = "2.0.0-alpha04"
 rootProject.description =
     "HijrahDateTime is a Kotlin Multiplatform library for working with the Hijrah calendar system. It provides a robust set of classes and functions to handle Hijrah dates and times across different platforms, integrating seamlessly with `kotlinx-datetime`."
 
